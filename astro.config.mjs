3f440c0e071e71dffac856aa2c0e888a6664450e
Marius Popa <m.popa@1xinternet.de>
2026-03-01 16:34:42 +0100

    #1: Update astro config for github page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://backatmarius.github.io',
+  base: '/funeral_astro/',
   vite: {
     plugins: [tailwindcss()]
   }
