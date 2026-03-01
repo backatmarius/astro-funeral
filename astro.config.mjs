c33ef699e348e7a70d32c04a198532c8f3ce6889
Marius Popa <m.popa@1xinternet.de>
2026-03-01 17:01:57 +0100

    update astro.yml

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://backatmarius.github.io',
-  base: '/funeral_astro/',
+  base: '/astro-funeral/',
   vite: {
     plugins: [tailwindcss()]
   }
